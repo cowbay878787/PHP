07009457f8eb34b56114bd24f84855efa7375429
cowbay878787 <93jerry0904@gmail.com>
2025-04-08 10:56:55 +0800

    Create 1.html.php

---
+++ 1.html.php
@@ -0,0 +1,11 @@
+<html>
+    <head><title>html+php</title></head>      
+    <body>
+        <?php echo "Hello world!"; ?> 
+        <h1>html</h1>
+        <?php echo "Hello world!"; ?> 
+        <h1>html</h1>
+        <?php echo "Hello world!"; ?> 
+    </body>
+</html>
+#h1為字體放大的效果

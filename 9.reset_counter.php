cb53e5e9917f3db7772c5f42f6e3a1586926f853
cowbay878787 <93jerry0904@gmail.com>
2025-04-08 11:42:34 +0800

    Update 9.reset_counter.php

--- 9.reset_counter.php
+++ 9.reset_counter.php
@@ -1,8 +1,8 @@
 <?php
-    session_start();
-    unset($_SESSION["counter"]);
-    echo "counter重置成功....";
-    echo "<meta http-equiv=REFRESH content='2; url=8.counter.php'>";
+    session_start(); #使用 session_start開始一個新的 session 或繼續當前的 session。操作 session 中儲存的資料。
+    unset($_SESSION["counter"]); #重置計數器
+    echo "counter重置成功...."; #在頁面上顯示counter重置成功，告訴用戶計數器已被重置。
+    echo "<meta http-equiv=REFRESH content='2; url=8.counter.php'>"; #這裡設置了 2 秒後跳轉到8.counter.php頁面。content='2; 表示 2 秒後會自動導向指定的 URL。
 
 ?>
  session 裡的 counter 變數清掉，然後自動在 2 秒後導回 8.counter.php

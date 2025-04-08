43af4228397e843eb24fb35b4284f8c3ff592959
cowbay878787 <93jerry0904@gmail.com>
2025-04-08 11:09:32 +0800

    Create 6.login.php

---
+++ 6.login.php
@@ -0,0 +1,19 @@
+<?php
+   #mysqli_connect() 建立資料庫連結
+   $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
+   #mysqli_query() 從資料庫查詢資料
+   $result=mysqli_query($conn, "select * from user");
+   #mysqli_fetch_array() 從查詢出來的資料一筆一筆抓出來
+   $login=FALSE;
+   while ($row=mysqli_fetch_array($result)) {
+     if (($_POST["id"]==$row["id"]) && ($_POST["pwd"]==$row["pwd"])) {
+       $login=TRUE;
+     }
+   } 
+   if ($login==TRUE)
+     echo "登入成功";
+  else
+     echo "帳號/密碼 錯誤";
+?>
+完整的登入驗證系統，而且是用資料庫來比對帳密，這段 PHP 程式會從 user 資料表抓出所有帳號密碼，然後比對使用者輸入的帳密，看看有沒有符合的一筆。
+

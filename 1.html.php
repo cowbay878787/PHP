8f6c24e3323f961a7a0e4b2832dcf62e675e8756
cowbay878787 <93jerry0904@gmail.com>
2025-04-08 11:22:22 +0800

    Update 1.html.php

--- 1.html.php
+++ 1.html.php
@@ -1,11 +1,11 @@
-<html>
-    <head><title>html+php</title></head>      
-    <body>
-        <?php echo "Hello world!"; ?> 
+<html> HTML 文件的開始。
+    <head><title>html+php</title></head>    包含了網頁的資料，標籤顯示的頁面標題：html+php
+    <body> 內容開始
+        <?php echo "Hello world!"; ?> ，輸出「Hello world！」這段文字
         <h1>html</h1>
-        <?php echo "Hello world!"; ?> 
+        <?php echo "Hello world!"; ?> 顯示一個主標題，內容是「html」
         <h1>html</h1>
-        <?php echo "Hello world!"; ?> 
+        <?php echo "Hello world!"; ?> 輸出「Hello world！」
     </body>
 </html>
-#h1為字體放大的效果
+#h1為字體放大的效果， HTML 和 PHP 結合在一起，並顯示多次「Hello world！」
